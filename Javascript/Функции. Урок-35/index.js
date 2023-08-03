0c94df71179e97f767b69fa22c6ae1ec1c8b1802
unknown <garippolosov1203@gmail.com>
2023-08-03 11:00:50 +0500

    Сделано практическая часть сравнивание let и var в примере

---
+++ Javascript/Функции. Урок-35/index.js
@@ -0,0 +1,8 @@
+let name = "John"
+
+if(10 > 3) {
+    let name = 'Doe'
+    console.log(name);
+}
+
+console.log(name)
---
+++ Javascript/Функции. Урок-35/index2.js
@@ -0,0 +1,8 @@
+var name = "John"
+
+if(10 > 3) {
+    var name = 'Doe'
+    console.log(name);
+}
+
+console.log(name)
