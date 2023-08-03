54df78fcbd8a479841d9bda57b68f3a2bb48c386
unknown <garippolosov1203@gmail.com>
2023-08-03 07:05:42 +0500

    Создано сайт который спрашивает у пользователя имю фамилию возраст и семейное положение

---
+++ Javascript/Javascript в Браузере. Урок-29/index.js
@@ -0,0 +1,6 @@
+name1 = prompt("What is your name?")
+surname = prompt("What is your surname?")
+age = prompt("How old are you?")
+job = prompt("What's your main job?")
+
+document.write(` ${name1}, ${surname}, ${age}, ${job}`)
