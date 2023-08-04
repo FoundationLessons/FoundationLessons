668c6f9a3416dd7866bbb4767a817be383b08fd1
unknown <garippolosov1203@gmail.com>
2023-08-04 19:58:52 +0500

    Создано 5 кастом иконок и сделано leaflet

---
+++ Leaflet/Custom Icons. Урок-52/index.js
@@ -0,0 +1,49 @@
+var map = L.map("map").setView([51.505, -0.09], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: "© OpenStreetMap",
+}).addTo(map);
+
+var greenIcon = L.icon({
+  iconUrl: "leaf-green.png",
+  shadowUrl: "leaf-shadow.png",
+
+  iconSize: [38, 95], // size of the icon
+  shadowSize: [50, 64], // size of the shadow
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62], // the same for the shadow
+  popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+});
+
+var orangeIcon = L.icon({
+  iconUrl: "leaf-orange.png",
+  shadowUrl: "leaf-shadow.png",
+
+  iconSize: [38, 95], // size of the icon
+  shadowSize: [50, 64], // size of the shadow
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62], // the same for the shadow
+  popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+});
+
+var redIcon = L.icon({
+  iconUrl: "leaf-red.png",
+  shadowUrl: "leaf-shadow.png",
+
+  iconSize: [38, 95], // size of the icon
+  shadowSize: [50, 64], // size of the shadow
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62], // the same for the shadow
+  popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+});
+
+
+
+L.marker([51.5, -0.09], { icon: greenIcon }).addTo(map);
+L.marker([51.6, -0.09], { icon: orangeIcon }).addTo(map);
+L.marker([51.7, -0.09], { icon: redIcon }).addTo(map);
+
+L.marker([51.5, -0.02], { icon: greenIcon }).addTo(map);
+L.marker([51.7, -0.12], { icon: redIcon }).addTo(map);
+L.marker([51.6, -0.04], { icon: orangeIcon }).addTo(map);
