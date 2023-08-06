c9d61d5b21bf2e92a2e557353cb14115d412d671
unknown <garippolosov1203@gmail.com>
2023-08-06 20:58:06 +0500

    Сделано первая часть практики

---
+++ ReactJs/Практика. Урок-64/my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyNavbar from "./Layouts/Navbar/MyNavbar"
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <MyNavbar/>
+    </div>
+  );
+}
+
+export default App;
