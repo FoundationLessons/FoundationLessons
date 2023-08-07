0366a42debd4cb68dd0c94fb60e5d3eeba705fe3
Jamoliddin <garippolosov1203@gmail.com>
2023-08-07 12:00:46 +0500

    Сделано свестка с помощью API war films

---
+++ ReactJs/Практика. Урок-67/my-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import WarMovies from './WarMovies';
+
+const App = () => {
+  return (
+    <div className="App">
+      <WarMovies/>
+    </div>
+  );
+}
+
+export default App;
